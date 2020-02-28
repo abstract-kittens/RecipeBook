cc61a9c923fca7a7ae483977c4915339bc1c7386
Your Name <tarskaya.alina@mail.ru>
2020-02-28 16:32:47 +0300

    Add, delete

--- handle_dialog.py
+++ handle_dialog.py
@@ -6,25 +6,48 @@ from add_recipe import *
 
 def handle_dialog(request, response, user_storage, db):
     if request.is_new_session:
-        user_storage = {"add recipe" : 0, "get recipe" : 0}
-        response.set_text('Hello, mthrfckr')
+        user_storage = {"add recipe" : 0, "get recipe" : 0,
+                        "delete recipe" : 0,
+                        "name" : None, "ingredients":None,
+                        "steps" : None}
+        response.set_text("Выберите действие")
+        response.set_buttons([
+                    {'title':'Добавь рецепт', 'hide':True},
+                    {'title':'Покажи рецепт', 'hide':True},
+                    {'title':'Измени рецепт', 'hide':True},
+                    {'title':'Удали рецепт', 'hide':True}
+                    ])
         return response, user_storage
     else:
-        
         if request.command.lower() == "добавь рецепт":
             user_storage["add recipe"] = 1
-            response.set_text("Назови ингредиенты в формате [название ингредиента] [количество] [мера]")
+            response.set_text("Скажите название рецепта")
             return response, user_storage
-        elif user_storage > 0:
+        elif user_storage["add recipe"] > 0:
             response, user_storage = add_recipe(request, response, user_storage, db)
             return response, user_storage
+        elif request.command.lower() == "удали рецепт":
+            user_storage["delete recipe"] = 1
+            response.set_text("Скажите название рецепта для удаления")
+            return response, user_storage
+        elif user_storage["delete recipe"] == 1:
+            user_storage["delete recipe"] = 0
+            delete_db(db, request.user_id, request.command.lower())
+            response.set_text("Рецепт успешно удален")
+            return response, user_storage
         else:
-            response.set_text("До встречи!")
+            response.set_text("Неизвестное действие, выберите то, что есть")
+            response.set_buttons([
+                    {'title':'Добавь рецепт', 'hide':True},
+                    {'title':'Покажи рецепт', 'hide':True},
+                    {'title':'Измени рецепт', 'hide':True},
+                    {'title':'Удали рецепт', 'hide':True}])
+            return response, user_storage
+'''            response.set_text("До встречи!")
             response.set_end_session(True)
             user_storage = {}
-
             return response, user_storage
-        return response, user_storage
+        return response, user_storage'''
 '''        elif request.command.lower() == "покажи в бд":
             user_storage[1] = 1
             response.set_text("Ок!")
