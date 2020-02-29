e8e7dafef095b611e1f4593446951dc0a47724f7
Ananas999 <saha0kotov@gmail.com>
2020-02-29 12:53:13 +0300

    show_recept

--- handle_dialog.py
+++ handle_dialog.py
@@ -3,6 +3,7 @@
 
 from __future__ import unicode_literals
 from add_recipe import *
+from show_recipe import *
 
 def handle_dialog(request, response, user_storage, db):
     if request.is_new_session:
@@ -26,6 +27,7 @@ def handle_dialog(request, response, user_storage, db):
         elif user_storage["add recipe"] > 0:
             response, user_storage = add_recipe(request, response, user_storage, db)
             return response, user_storage
+        
         elif request.command.lower() == "удали рецепт":
             user_storage["delete recipe"] = 1
             response.set_text("Скажите название рецепта для удаления")
@@ -35,10 +37,19 @@ def handle_dialog(request, response, user_storage, db):
             delete_db(db, request.user_id, request.command.lower())
             response.set_text("Рецепт успешно удален")
             return response, user_storage
-        elif request.command.lower() == "измени рецепт":
-             user_storage["get recipe"] = 3
+        
+        elif request.command.lower() == "покажи рецепт":
+             user_storage["get recipe"] = 1
              response.set_text("Скажите название рецепта, который хотите воспроизвести")
              return response, user_storage
+        elif user_storage["get recipe"] > 0:
+             response, user_storage = show_recipe(request, response, user_storage, db)
+             return response, user_storage
+         
+        elif request.command.lower() == "помощь":
+            response.set_text("""я могу добавить новый рецепт, удалить существующий,
+                              изменить ингредиенты или воспроизвести один из ваших рецептов""")
+            return response, user_storage
         else:
             response.set_text("Неизвестное действие, выберите то, что есть")
             response.set_buttons([
